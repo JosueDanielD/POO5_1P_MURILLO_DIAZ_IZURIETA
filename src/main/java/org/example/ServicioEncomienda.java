df71fba320f009331154ab07fb26afad99b7bb0b
Josue Diaz <josdbaqu@espol.edu.ec>
2023-11-25 22:28:10 -0500

    Implementación de métodos en la clase Servicio y sobreescritura en sus clases hijas

--- src/main/java/org/example/ServicioEncomienda.java
+++ src/main/java/org/example/ServicioEncomienda.java
@@ -1,15 +1,19 @@
 package org.example;
 
-public class ServicioEncomienda {
+import java.util.Random;
+
+public class ServicioEncomienda extends Servicio{
     private int cantidadProductos;
     private double peso;
     private TipoServicio tipoServicio;
     private TipoEncomienda tipoEncomienda;
 
 
-    //@Override
-    //public double calcularCosto(){
+    @Override
+    public double calcularCosto(){
+        double costoEncomienda=this.cantidadProductos+4;
+        return costoEncomienda;
 
 
-    //}
+    }
 }
--- src/main/java/org/example/ServicioTaxi.java
+++ src/main/java/org/example/ServicioTaxi.java
@@ -4,19 +4,13 @@ public class ServicioTaxi extends Servicio {
     private int numeroPersonas;
     private TipoServicio tipoServicio;
 
-    //@Override
-    public double calcularCosto(MetodoPago metodoPago){
+    @Override
+    public double calcularCosto(){
         Random r= new Random();
         int kilometraje=r.nextInt(4,45);
         final double tarifa=0.50;
         double costoViaje= kilometraje*tarifa;
-        if (metodoPago==MetodoPago.TC){
-            costoViaje=costoViaje+(costoViaje*0.15);
-
-        }
 
         return costoViaje;
-
-
     }
 }
