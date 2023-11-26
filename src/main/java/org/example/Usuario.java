02da939f0c2c516657a7047150a3f7d81763c306
eriamuri@espol.edu.ec <Hp@Erick>
2023-11-25 23:05:19 -0500

    La clase Usuario, constructor y sus metodos.

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,23 +1,25 @@
 package org.example;
 
-public abstract class Usuario {
+public class Usuario {
 
     //(em)ATRIBUTOS CLASE USUARIO
     protected String nombre;
     protected String apellido;
     protected String numeroCedula;
     protected String user;
+    protected String contraseña;
     protected String numeroCelular;
     protected TipoUsuario tipoUsuario;
 
     //(em)CONSTRUCTOR DE USUARIO
-    public Usuario(String nombre,String apellido,String numeroCedula, String user,String numeroCelular, TipoUsuario tipoUsuario){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.numeroCedula=numeroCedula;
-        this.user=user;
-        this.numeroCelular=numeroCelular;
-        this.tipoUsuario=tipoUsuario;
+    public Usuario(String nombre,String apellido,String numeroCedula, String user,String contraseña,String numeroCelular, TipoUsuario tipoUsuario){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroCedula = numeroCedula;
+        this.user = user;
+        this.contraseña = contraseña;
+        this.numeroCelular = numeroCelular;
+        this.tipoUsuario = tipoUsuario;
     }
 
     //(em)CONSTRUCTOR VACÍO:
@@ -27,7 +29,8 @@ public abstract class Usuario {
 
 
     //(em)MÉTODOS USUARIO
-    public abstract void consultarServicios();
+    public void consultarServicios(){
+    }
 
     
 
