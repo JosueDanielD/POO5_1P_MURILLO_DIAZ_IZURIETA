131a4bdad93b0010bd8809aa75924c270bc4b01f
eriamuri@espol.edu.ec <Hp@Erick>
2023-11-25 23:06:00 -0500

    La clase hija "Cliente" con su constructor y metodos.

--- src/main/java/org/example/Cliente.java
+++ src/main/java/org/example/Cliente.java
@@ -1,9 +1,40 @@
 package org.example;
 
 public class Cliente extends Usuario {
+    
+    //(em) Atributos de la clase hija "cliente"
     private int edad;
     private String numeroTarjeta;
-
+    
+    //(em) constructor de Cliente
+    public Cliente(String nombre,
+            String apellido,
+            String numeroCedula,
+            String user,
+            String contraseña,
+            String numeroCelular,
+            TipoUsuario tipoUsuario,
+            int edad,
+            String numeroTarjeta){
+    
+        super(nombre,apellido,numeroCedula,user,contraseña,numeroCelular,tipoUsuario);
+        this.edad = edad;
+        this.numeroTarjeta = numeroTarjeta;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    public String getNumeroTarjeta(){
+        return numeroTarjeta;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public void setNumeroTarjeta(String numeroTarjeta){
+        this.numeroTarjeta = numeroTarjeta;
+    }
 
     //@Override
     //public void consultarServicios(){
