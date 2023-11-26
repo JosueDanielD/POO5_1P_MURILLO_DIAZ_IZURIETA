00a52392464b7d2954d8fdcead1d24127c1b9dbe
eriamuri@espol.edu.ec <Hp@Erick>
2023-11-25 22:43:12 -0500

    La clase Usuario es abstracta y su metodo consultar servicios

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,8 +1,8 @@
 package org.example;
 
-public class Usuario {
+public abstract class Usuario {
 
-    //(jd)ATRIBUTOS CLASE USUARIO
+    //(em)ATRIBUTOS CLASE USUARIO
     protected String nombre;
     protected String apellido;
     protected String numeroCedula;
@@ -10,7 +10,7 @@ public class Usuario {
     protected String numeroCelular;
     protected TipoUsuario tipoUsuario;
 
-    //(jd)CONSTRUCTOR DE USUARIO
+    //(em)CONSTRUCTOR DE USUARIO
     public Usuario(String nombre,String apellido,String numeroCedula, String user,String numeroCelular, TipoUsuario tipoUsuario){
         this.nombre=nombre;
         this.apellido=apellido;
@@ -20,64 +20,16 @@ public class Usuario {
         this.tipoUsuario=tipoUsuario;
     }
 
-    //(jd)CONSTRUCTOR VACÍO:
+    //(em)CONSTRUCTOR VACÍO:
 
     public Usuario(){
-
     }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    //(em)MÉTODOS USUARIO
+    public abstract void consultarServicios();
 
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getNumeroCedula() {
-        return numeroCedula;
-    }
-
-    public void setNumeroCedula(String numeroCedula) {
-        this.numeroCedula = numeroCedula;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getNumeroCelular() {
-        return numeroCelular;
-    }
-
-    public void setNumeroCelular(String numeroCelular) {
-        this.numeroCelular = numeroCelular;
-    }
-
-    public TipoUsuario getTipoUsuario() {
-        return tipoUsuario;
-    }
-
-    public void setTipoUsuario(TipoUsuario tipoUsuario) {
-        this.tipoUsuario = tipoUsuario;
-    }
-
-    //(jd)MÉTODOS USUARIO
-    public void consultarServicios(){
-
-    }
+    
 
 
 
