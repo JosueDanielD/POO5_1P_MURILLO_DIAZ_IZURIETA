84af39add4a338e060702739c422e4da122d8018
domaizur <domaizur@espol.edu.ec>
2023-11-26 12:45:49 -0500

    lo mio

--- src/main/java/org/example/Vehiculo.java
+++ src/main/java/org/example/Vehiculo.java
@@ -5,4 +5,38 @@ public class Vehiculo {
     private String marca;
     private String modelo;
     private TipoVehiculo tipoVehiculo;
+    
+    
+    
+    public Vehiculo(String placa, String marca, String modelo, TipoVehiculo tipoVehiculo ){
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipoVehiculo = tipoVehiculo;
+    }
+    
+    public String getplaca(){
+        return placa;
+    }
+    public String getmarca(){
+        return marca;
+    }
+    public String getmodelo(){
+        return modelo;
+    }
+    public TipoVehiculo gettipoVehiculo(){
+        return  tipoVehiculo;
+    }
+    public void setplaca(String placa){
+        this.placa = placa;
+    }
+    public void setmarca(String marca){
+        this.marca = marca;
+    }
+    public void setmodelo(String modelo){
+        this.modelo = modelo;
+    }
+    public void settipoVehiculo(TipoVehiculo tipoVehiculo){
+        this.tipoVehiculo = tipoVehiculo;
+    }
 }
